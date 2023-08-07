76438bdc35a28c94b7436fd8982f3ae682f3a9ba
hungcq <hungcq1996@gmail.com>
2023-08-07 22:15:25 +0700

    aggregate log with fluentd sidecar, opensearch & opensearch dashboard

--- book-service/cmd/main.go
+++ book-service/cmd/main.go
@@ -4,9 +4,19 @@ import (
 	"book-service/internal/business_logic"
 	"book-service/internal/inbound"
 	"book-service/internal/outbound"
+	"log"
+	"os"
 )
 
 func main() {
+	f, err := os.OpenFile("/var/log/data.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	outbound.InitMySqlDb()
 	outbound.InitHttpClient()
 
