8e6652e273eba7fed71aca6ba0c2189f4c1edc8e
hungcq <hungcqrt@gmail.com>
2025-06-10 23:35:03 +0700

    fix missing timezone

--- backend/internal/main.go
+++ backend/internal/main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	config2 "github.com/hungcq/pscit/backend/internal/config"
-	"github.com/hungcq/pscit/backend/internal/routes"
 	"log"
+	_ "time/tzdata"
+
+	"github.com/hungcq/pscit/backend/internal/config"
+	"github.com/hungcq/pscit/backend/internal/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Initialize database
-	db := config2.InitDB()
+	db := config.InitDB()
 
 	// Create Gin router
 	r := gin.Default()
 
 	// Setup CORS
-	r.Use(config2.CORSMiddleware())
+	r.Use(config.CORSMiddleware())
 
 	// Initialize routes
 	routes.SetupRoutes(r, db)
 
 	// Start server
-	if err := r.Run(":" + config2.AppConfig.Port); err != nil {
+	if err := r.Run(":" + config.AppConfig.Port); err != nil {
 		log.Fatal("Failed to start server:", err)
 	}
 }
