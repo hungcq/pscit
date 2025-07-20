aaee1bceb495167315dbfea11d74d919fc1726fd
hungcq <hungcqrt@gmail.com>
2025-07-20 12:39:43 +0700

    update title

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" title="PSciT Library">
       <Head />
       <body>
         <Main />
